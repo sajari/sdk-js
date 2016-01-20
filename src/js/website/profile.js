0f0c4f40226bff06b09cc4be41e3c956bbf7aaf0
Hamish <hamish.ogilvy@gmail.com>
2016-01-20 17:26:22 +1100

    Profile functionality broken into its own module

---
+++ src/js/website/profile.js
@@ -0,0 +1,55 @@
+/*
+ * profile.js - Handles Sajari profile information
+ */
+var cookie = require("./cookie");
+
+/**
+ * Profile constructor
+ */
+function profile(options) {
+	options = options || {};
+	if (options.meta === undefined) {
+		this.meta = {};
+	}
+	this.gaId = getGAID();
+
+	if (this.visitorId === undefined) {
+        this.visitorId = cookie.get('sjID');
+        if (this.visitorId === undefined) {
+            this.visitorId = (new Date().getTime()) + '.' + Math.floor(Math.random() * 1000000);
+        }
+        cookie.set('sjID', this.visitorId, 365);
+    }
+
+	this.options = options;
+}
+
+profile.prototype = {
+	/**
+	 * Sets a hardcoded visitor identifier
+	 */
+	setVisitorId : function(id) {
+	    if (id !== undefined) {
+	        this.visitorId = id + ''
+	        cookie.set('sjID', this.visitorId, 365);
+	    }
+	    return this.visitorId;
+	}
+}
+
+/**
+ * Returns the current visitors Google Analytics ID if it exists
+ */
+function getGAID() {
+    var userId = '';
+    var gaUserCookie = cookie.get("_ga"); 
+    if (gaUserCookie !== undefined) { 
+        var cookieValues = gaUserCookie.split('.');
+        if (cookieValues.length > 2 ) { 
+            userId = cookieValues[2]; 
+        }  
+    } 
+    return userId;
+}
+
+module.exports = profile; 
