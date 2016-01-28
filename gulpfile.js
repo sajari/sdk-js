bb405f9521a021b0609106fbe0c168b5bfc69d8c
Hamish <hamish.ogilvy@gmail.com>
2016-01-28 12:03:26 +1100

    Add "tape" testing into gulp. Also add "watch" for live changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
+var tape = require('gulp-tape');
+var tapColorize = require('tap-colorize');
 var exec = require('child_process').exec;
 
 var browserify = require('browserify');
@@ -11,6 +14,13 @@ var buffer = require('vinyl-buffer');
 function handleErrors(errors) {
 	console.log(errors);
 }
+
+gulp.task('test', function() {
+  return gulp.src('tests/*.js')
+    .pipe(tape({
+      reporter: tapColorize()
+    }));
+});
 
 gulp.task('browserify', function() {
     return browserify('./src/js/api.js')
@@ -28,5 +38,10 @@ gulp.task('uglify', function() {
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['browserify', 'uglify']);
+gulp.task('default', ['test', 'browserify', 'uglify']);
 
+gulp.task("watch", function() {
+    watch(['src/js/*', 'tests/*'], function() {
+        gulp.start("default");
+    });
+});
