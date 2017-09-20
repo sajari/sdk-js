08e38a5e3c216b59130e5c47c928a94fa2d88bf3
Trent Billington <pudgepacket@gmail.com>
2017-09-20 12:36:18 +1000

    test: add test case covering setting and getting custom metadata

--- src/sajari.test.js
+++ src/sajari.test.js
@@ -1,6 +1,14 @@
-import { userAgent } from "./sajari";
+import { userAgent, Client } from "./sajari";
 
 test("package version in code matches package", () => {
   const packageVersion = require("../package.json").version;
   expect(userAgent).toBe("sdk-js-" + packageVersion);
+});
+
+test("setting and getting custom metadata", () => {
+  const client = new Client("", "");
+  const key = "foo";
+  const value = ["bar"];
+  client.setCustomMetadata({ [key]: value });
+  expect(client.getMetadata()).toHaveProperty(key, value);
 });
