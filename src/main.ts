4cc1afac10089f423534f12453b4c48d69bf5090
Trent Billington <pudgepacket@gmail.com>
2018-03-13 18:12:37 +1100

    client: start implementation

--- src/main.ts
+++ src/main.ts
@@ -1,245 +1,278 @@
 /**
  * @fileOverview Exports the Sajari javascript api.
- * @name sajari.js
+ * @name main.ts
  * @author Sajari
  * @license MIT
  * @module sajari
  */
-// import profile from "sajari-website/src/js/profile";
 
-export const userAgent = "sdk-js-0.20.0";
+export interface Tracking {
+	// Tracking specifies which kind (if any) tokens should be generated and returned
+	// with the query results.
+	type: TrackingType;
 
-const makeError = (message: string, code?: number) => ({
-  message,
-  code
-});
+	// QueryID is a unique identifier for a single search query.  In the
+	// case of live querying this is defined to be multiple individual queries
+	// (i.e. as a user types the query is re-run).
+	queryID: string;
 
-const makeRequest = (address: string, body: any, callback: any) => {
-  const request = new XMLHttpRequest();
-  request.open("POST", address, true);
-  request.setRequestHeader("Accept", "application/json");
-  request.onreadystatechange = () => {
-    if (request.readyState !== 4) return;
+	// Sequence (i.e. sequential identifier) of this  in the context of a
+	// sequence of queries.
+	sequence: number;
 
-    let parsedResponse;
-    try {
-      parsedResponse = JSON.parse(request.responseText);
-    } catch (e) {
-      const error = makeError("error parsing response");
-      callback(error, null);
-      return;
-    }
+	// Field is the field to be used for adding identifier information to
+	// generated tokens (see TrackingType).
+	field: string;
 
-    if (request.status === 200) {
-      callback(null, parsedResponse);
-      return;
-    }
+	// Data are values which will be recorded along with tracking data produced
+	// for the request.
+	data: Values;
+}
 
-    const error = makeError(parsedResponse.message, request.status);
-    callback(error, null);
-  };
-  request.send(body);
-  return request;
-};
+export interface Values {
+  [id: string]: string;
+}
 
-const makeSearchResponse = (json: any) => {
-  // Flatten single value / multiple values proto structure
-  const r = (json.searchResponse || {}).results;
-  if (r) {
-    for (let i = 0; i < r.length; i++) {
-      for (let f in r[i].values) {
-        r[i].values[f] =
-          r[i].values[f].single !== undefined
-            ? r[i].values[f].single
-            : r[i].values[f].repeated.values;
-      }
-      // Copy tokens into results
-      if (json.tokens) {
-        r[i].tokens = json.tokens[i];
-      }
-    }
-  } else {
-    // Set default proto values for empty response
-    json.searchResponse = {
-      results: [],
-      time: (json.searchResponse || {}).time,
-      totalResults: "0",
-      reads: "0"
-    };
-  }
-  return json;
-};
+export interface Session {
+  next(values: Values): [Tracking | undefined, Error | undefined];
+  reset(): void;
+}
 
-/** Class representing an instance of the Client. Handles the searching of queries and keeping track of query id and sequence */
-export class Client {
-  p: string;
-  c: string;
-  e: string;
-  /**
-   * Creates an Client object.
-   * @constructor
-   * @param {string} project The project name.
-   * @param {string} collection The collection name.
-   * @param {string} [endpoint] A custom endpoint to send requests.
-   * @returns {Client} Client object.
-   */
-  constructor(project: string, collection: string, endpoint: string = "https://jsonapi.sajari.net") {
-    /** @private */
-    this.p = project;
+export const TrackingNone: string = "";
+export const TrackingClick: string = "CLICK";
+export const TrackingPosNeg: string = "POS_NEG";
 
-    /** @private */
-    this.c = collection;
+export const enum TrackingType {
+  TrackingNone = "",
+  TrackingClick = "CLICK",
+  TrackingPosNeg = "POS_NEG",
+}
 
-    /** @private */
-    this.e = endpoint;
-  }
-
-  /**
-   * Performs a search
-   * @param {Query} query The query object to perform a search with.
-   * @param {Tracking} tracking The tracking config for the search.
-   * @param {function(err: string, res: Object)} callback The callback to call when a response is received.
-   * @returns {XMLHttpRequest}
-   */
-  search(query: any, tracking: any, callback: any) {
-    const requestBody = JSON.stringify({
-      request: {
-        searchRequest: query.q,
-        // eslint-disable-next-line no-param-reassign
-        tracking: {
-          type: tracking.type,
-          field: tracking.field,
-          sequence: tracking.s++, // Increment query sequence
-          query_id: tracking.i,
-          data: tracking.data
-        }
-      },
-      metadata: {
-        project: [this.p],
-        collection: [this.c],
-        "user-agent": [userAgent]
-      }
-    });
-
-    return makeRequest(
-      this.e + "/sajari.api.query.v1.Query/Search",
-      requestBody,
-      (err: any, response: any) => {
-        if (err) {
-          callback(err, null);
-          return;
-        }
-        callback(null, makeSearchResponse(response));
-      }
+// randString constructs a random string of 16 characters.
+const randString = (): string => {
+  let queryID = ""
+  for (let i = 0; i < 16; i++) {
+    queryID += "abcdefghijklmnopqrstuvwxyz0123456789".charAt(
+      Math.floor(Math.random() * 36)
     );
   }
+  return queryID;
+}
 
-  /**
-   * Performs a search using a pipeline.
-   * @param {string} name The name of the pipeline to search with.
-   * @param {Object} values Value map for the pipeline.
-   * @param {Tracking} tracking The tracking config for the search.
-   * @param {function(err: string, res: Object)} callback The callback to call when a response is received.
-   * @returns {XMLHttpRequest}
-   */
-  searchPipeline(name: string, values: any, tracking: any, callback: any) {
-    const stringifiedValues: any = {};
-    Object.keys(values).forEach(k => {
-      stringifiedValues[k] = String(values[k]);
-    });
+export class WebSearchSession implements Session {
+  private queryLabel: string;
+  private session: Session;
+  private lastQuery: string = "";
 
-    const requestBody = JSON.stringify({
-      request: {
-        pipeline: { name },
-        // eslint-disable-next-line no-param-reassign
-        tracking: {
-          type: tracking.type,
-          field: tracking.field,
-          sequence: tracking.s++, // Increment query sequence
-          query_id: tracking.i,
-          data: tracking.data
-        },
-        values: stringifiedValues
-      },
-      metadata: {
-        project: [this.p],
-        collection: [this.c],
-        "user-agent": [userAgent]
-      }
-    });
+  public constructor(queryLabel: string, session: Session) {
+    this.queryLabel = queryLabel;
+    this.session = session;
+  }
+  
+  public next(values: Values): [Tracking | undefined, Error | undefined] {
+    const text = values[this.queryLabel];
+    if (text === undefined) {
+      this.reset();
+      return this.session.next(values);
+    }
+    
+    if (text !== this.lastQuery) {
+        this.reset();
+    }
 
-    return makeRequest(
-      this.e + "/sajari.api.pipeline.v1.Query/Search",
-      requestBody,
-      (err: any, response: any) => {
-        if (err) {
-          callback(err, null);
-          return;
-        }
-        callback(null, makeSearchResponse(response));
-      }
-    );
+    return this.session.next(values);
+  }
+  
+  public reset(): void {
+    this.session.reset();
   }
 }
 
-export const clickTracking = "CLICK";
-export const posNeg = "POS_NEG";
+export class SessionType implements Session {
+  private queryID: string = "";
+  private sequence: number = 0;
 
-export class Tracking {
-  type: string|undefined;
-  field: string|undefined;
-  i: string = "";
-  s: number = 0;
-  /**
-   * Create a new Tracking object.
-   * @param {string|undefined} [type=undefined] Type of tracking to be used.
-   * @param {string|undefined} [field=undefined] Unique field to use for tracking.
-   */
-  constructor(type = undefined, field = undefined) {
-    /** @private */
-    this.type = type;
-    /** @private */
+  private trackingType: TrackingType;
+  private field: string;
+  private sessionData: Values;
+
+  public constructor(trackingType: TrackingType, field: string, data: Values) {
+    this.trackingType = trackingType;
     this.field = field;
-
-    this.reset();
-
-    /** @private */
-    // this.data = {}; // tracking data
-
-    // const p = new profile();
-    // const gaID = p.gaId;
-    // if (gaID) {
-    //   this.setData("gaID", gaID);
-    // }
-    // const visitorID = p.visitorId;
-    // if (visitorID) {
-    //   this.setData("sjID", visitorID);
-    // }
+    this.sessionData = data;
   }
 
-  /**
-   * Resets tracking ID on the Query object.
-   */
-  reset() {
-    /** @private */
-    this.i = "";
-    /** @private */
-    this.s = 0;
-
-    // Generate a random id for the query
-    for (let i = 0; i < 16; i++) {
-      this.i += "abcdefghijklmnopqrstuvwxyz0123456789".charAt(
-        Math.floor(Math.random() * 36)
-      );
+  public next(): [Tracking | undefined, Error | undefined] {
+    if (this.queryID === "") {
+      this.queryID = randString();
+      this.sequence = 0;
+    } else {
+      this.sequence++;
     }
+
+    return [{
+      type: this.trackingType,
+      queryID: this.queryID,
+      sequence: this.sequence,
+      field: this.field,
+      data: this.sessionData
+    }, undefined];
+  }
+
+  public reset(): void {
+    this.queryID = "";
+    this.sequence = 0;
+  }
+}
+
+export class Client {
+  public project: string;
+  public collection: string;
+  public endpoint: string;
+
+  public constructor(project: string, collection: string) {
+    this.project = project;
+    this.collection = collection;
+    this.endpoint = "";
+  }
+
+  public pipeline(name: string): Pipeline {
+    return new Pipeline(this, name);
+  }
+}
+
+export interface ResultValues {
+  [id: string]: any;
+}
+
+export interface TokenValues {
+  [id: string]: any;
+}
+
+export interface Result {
+  // Values are field values of records.
+	values: ResultValues;
+
+	// Tokens contains any tokens associated with this Result.
+	tokens: TokenValues;
+
+	// Score is the overall score of this Result.
+	score: number;
+
+	// IndexScore is the index-matched score of this Result.
+	indexScore: number;
+}
+
+export interface AggregateValues {
+  [id: string]: any;
+}
+
+export interface Results {
+  // Reads is the total number of index values read.
+	reads: number;
+
+	// TotalResults is the total number of results for the query.
+	totalResults: number;
+
+	// Time taken to perform the query.
+	time: string;
+
+	// Aggregates computed on the query results (see Aggregate).
+	aggregates: AggregateValues;
+
+	// Results of the query.
+	results: Result[];
+}
+
+export interface Key {
+	field: string;
+	value: any;
+}
+
+export type SearchCallback = (results: Results, values: Values, error: Error ) => void;
+
+export type AddCallback = (key: Key, error: Error ) => void;
+
+export interface SJRecord {
+  [id: string]: any;
+}
+
+/**
+ * Pipeline is a client for performing searches and adds on a collection.
+ */
+export class Pipeline {
+  private client: Client;
+  private name: string;
+
+  /**
+   * Create a pipeline
+   */
+  public constructor(client: Client, name: string) {
+    this.client = client;
+    this.name = name;
   }
 
   /**
-   * Set tracking data in a key value stre to be sent with the Query.
-   * @param {string} name The name of the custom tracking data
-   * @param {string} value The value of the custom tracking data
+   * Search runs a search query defined by a pipline with the given values and
+   * tracking configuration. Returns the query results and returned values (which could have
+   * been modified in the pipeline).
    */
-  setData(name: string, value: string) {
-    // this.data[name] = value;
+  public search(values: Values, tracking: Tracking, callback: SearchCallback): void {
+    const request = new XMLHttpRequest();
+    request.open("POST", this.client.endpoint, true);
+    request.setRequestHeader("Accept", "application/json");
+    request.onreadystatechange = () => {
+      if (request.readyState !== 4) return;
+  
+      let parsedResponse;
+      try {
+        parsedResponse = JSON.parse(request.responseText);
+      } catch (e) {
+        // const error = makeError("error parsing response");
+        // callback(error, null);
+        return;
+      }
+  
+      if (request.status === 200) {
+        // callback(null, parsedResponse);
+        return;
+      }
+  
+      // const error = makeError(parsedResponse.message, request.status);
+      // callback(error, null);
+    };
+    request.send(values);
+    //return request;
+  }
+
+  /**
+   * Add a record to a collection using a pipeline, returning the unique key which can be used
+   * to retrieve the respective record.
+   */
+  public add(values: Values, record: SJRecord, callback: AddCallback): void {
+    const request = new XMLHttpRequest();
+    request.open("POST", this.client.endpoint, true);
+    request.setRequestHeader("Accept", "application/json");
+    request.onreadystatechange = () => {
+      if (request.readyState !== 4) return;
+  
+      let parsedResponse;
+      try {
+        parsedResponse = JSON.parse(request.responseText);
+      } catch (e) {
+        // const error = makeError("error parsing response");
+        // callback(error, null);
+        return;
+      }
+  
+      if (request.status === 200) {
+        // callback(null, parsedResponse);
+        return;
+      }
+
+      // const error = makeError(parsedResponse.message, request.status);
+      // callback(error, null);
+    };
+    request.send(values);
   }
 }
