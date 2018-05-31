0ae245c47359cde002ba9321ac3e2cbf6bfc506a
Trent Billington <pudgepacket@gmail.com>
2018-05-31 12:44:56 +1000

    docs: test session types better

--- src/session.test.ts
+++ src/session.test.ts
@@ -1,26 +1,45 @@
 import { BaseSession, TextSession, TrackingType } from "./session";
 
 describe("Session", () => {
-  test("Session", () => {
-    const s = new BaseSession(TrackingType.None, "", {});
-    let [_, err] = s.next({});
-    expect(err).toBeUndefined();
-    [_, err] = s.next({});
-    expect(err).toBeUndefined();
-    s.reset();
+  describe("BaseSession", () => {
+    test("BaseSession", () => {
+      const s = new BaseSession(TrackingType.None, "", {});
+      let [_, err] = s.next({});
+      expect(err).toBeUndefined();
+      [_, err] = s.next({});
+      expect(err).toBeUndefined();
+      s.reset();
+    });
   });
 
-  test("TextSession", () => {
-    const s = new TextSession("q", new BaseSession(TrackingType.Click, "", {}));
-    let [values, err] = s.next({ q: "foo" });
-    console.log({ type: values.type, trackingType: TrackingType.Click });
-    expect(values.type).toBe(TrackingType.Click);
-    let qid = values.query_id;
-    expect(err).toBeUndefined();
-    [values, err] = s.next({ q: "" });
-    expect(values.query_id).not.toBe(qid);
-    qid = values.query_id;
-    expect(err).toBeUndefined();
-    s.reset();
+  describe("TextSession", () => {
+    test.each([
+      ["foo", undefined, true],
+      ["foo", "woo", true],
+      ["foo", "", true],
+      ["foof", "foow", false],
+      ["", "foo", false],
+      ["foo", "fo", false]
+    ])(
+      `TextSession changing query value from %o to %o resets: %o`,
+      (from, to, reset) => {
+        const s = new TextSession(
+          "q",
+          new BaseSession(TrackingType.Click, "", {})
+        );
+        let [values, err] = s.next({ q: from });
+        expect(err).toBeUndefined();
+        let qid = values.query_id;
+
+        [values, err] = s.next({ q: to });
+        expect(err).toBeUndefined();
+
+        if (reset) {
+          expect(values.query_id).not.toBe(qid);
+        } else {
+          expect(values.query_id).toBe(qid);
+        }
+      }
+    );
   });
 });
