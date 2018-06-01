d3cea31640d433d0739799e50f2082fb7cb7d26f
Trent Billington <pudgepacket@gmail.com>
2018-06-01 17:19:48 +1000

    tests: add tests for constructors

--- src/constructors.test.ts
+++ src/constructors.test.ts
@@ -11,11 +11,15 @@ describe("valueFromProto", () => {
     const protoValue = { single: value };
     expect(valueFromProto(protoValue)).toBe(value);
   });
-
   test("repeated", () => {
     const values = ["foobar"];
     const protoValue = { repeated: { values } };
     expect(valueFromProto(protoValue)).toBe(values);
+  });
+  test("null", () => {
+    const value = null;
+    const protoValue = { null: true };
+    expect(valueFromProto(protoValue)).toBe(value);
   });
 });
 
@@ -30,7 +34,8 @@ describe("newResult", () => {
       values: {
         description: { single: description },
         title: { single: title },
-        url: { single: url }
+        url: { single: url },
+        foobar: { null: true }
       },
       score,
       indexScore
@@ -45,14 +50,29 @@ describe("newResult", () => {
 });
 
 describe("newAggregates", () => {
+  test("bucket", () => {
+    const name = "bucket.somefield";
+    const buckets = { somebucket: { name: "some", count: 5 } };
+    const protoAggregate = { [name]: { buckets: { buckets } } };
+    expect(newAggregates(protoAggregate)).toEqual({ [name]: buckets });
+  });
   test("count", () => {
     const name = "count.category";
     const counts = { A: 1, B: 2, C: 3 };
     const protoAggregate = { [name]: { count: { counts } } };
     expect(newAggregates(protoAggregate)).toEqual({ [name]: counts });
   });
-  test("bucket", () => {
-    // ?
+  test("date", () => {
+    const name = "date.date_issued";
+    const dates = { "2017": 24, "2018": 25 };
+    const protoAggregate = { [name]: { date: { dates } } };
+    expect(newAggregates(protoAggregate)).toEqual({ [name]: dates });
+  });
+  test("metric", () => {
+    const name = "metric.somefield";
+    const value = 5;
+    const protoAggregate = { [name]: { metric: { value } } };
+    expect(newAggregates(protoAggregate)).toEqual({ [name]: value });
   });
 });
 
@@ -92,5 +112,33 @@ describe("processSearchResponse", () => {
       indexScore,
       token: { click: tokenValue }
     });
+  });
+  test("posNeg tokens", () => {
+    const protoResponse = {
+      results: [{ values: {}, score: 0, indexScore: 0 }]
+    };
+    const token = { pos: "a", neg: "b" };
+    const protoTokens = [{ posNeg: token }];
+    expect(
+      processSearchResponse(protoResponse, protoTokens).results
+    ).toContainEqual({ values: {}, score: 0, indexScore: 0, token });
+  });
+  test("no tokens", () => {
+    const protoResponse = {
+      results: [{ values: {}, score: 0, indexScore: 0 }]
+    };
+    expect(
+      processSearchResponse(protoResponse, []).results
+    ).toContainEqual({ values: {}, score: 0, indexScore: 0, token: {} });
+  });
+  test("unknown token type not erroring", () => {
+    const protoResponse = {
+      results: [{ values: {}, score: 0, indexScore: 0 }]
+    };
+    const token = { magic: "type" };
+    const protoTokens = [{ newTokenType: token }];
+    expect(
+      processSearchResponse(protoResponse, protoTokens).results
+    ).toContainEqual({ values: {}, score: 0, indexScore: 0, token: {} });
   });
 });
