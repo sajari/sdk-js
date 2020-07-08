d6b269bb7fe764aca0842c44dc5b987d0c73fb45
Sam Potts <sam@potts.es>
2020-07-08 20:31:10 +1000

    Use icons from @sajari-ui

--- examples/ecommerce/preact.config.js
+++ examples/ecommerce/preact.config.js
@@ -4,12 +4,5 @@ export default {
     // Remove PostCSS config
     const postcssLoader = helpers.getLoadersByName(config, 'postcss-loader');
     postcssLoader.forEach(({ loader }) => delete loader.options);
-
-    // Add custom babel plugins
-    const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
-    const babelConfig = rule.options;
-
-    // Inline SVGs
-    babelConfig.plugins.push('inline-react-svg');
   },
 };
