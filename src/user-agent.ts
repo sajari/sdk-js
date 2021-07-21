8ad7bd3c9027d1a04384c24721ed352c0534ca15
Wesley Walser <wwalser@sajari.com>
2021-07-22 00:15:27 +1000

    pkg: release 2.0.1

--- src/user-agent.ts
+++ src/user-agent.ts
@@ -16,6 +16,6 @@ if (scriptTag) {
  * user agent of sdk
  * @hidden
  */
-export const USER_AGENT = ["sajari-sdk-js/2.0.0", suffix]
+export const USER_AGENT = ["sajari-sdk-js/2.0.1", suffix]
   .filter(Boolean)
   .join(" ");
