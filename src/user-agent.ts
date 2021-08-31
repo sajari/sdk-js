1b9daa7c8f215f6efe7e350445110219ed7ceca5
Wesley Walser <wwalser@sajari.com>
2021-08-31 14:25:18 +1000

    chore: releasing 2.1.1 so that others may benefit from my labour.

--- src/user-agent.ts
+++ src/user-agent.ts
@@ -16,6 +16,6 @@ if (scriptTag) {
  * user agent of sdk
  * @hidden
  */
-export const USER_AGENT = ["sajari-sdk-js/2.0.2", suffix]
+export const USER_AGENT = ["sajari-sdk-js/2.1.1", suffix]
   .filter(Boolean)
   .join(" ");
