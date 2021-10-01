f16112c085c8f56f507d9d05f3e40531a82730f1
Wesley Walser <wwalser@sajari.com>
2021-10-01 18:40:51 +1000

    feat: redirect response shape has been changed to better afford for search client implementation

--- src/user-agent.ts
+++ src/user-agent.ts
@@ -16,6 +16,6 @@ if (scriptTag) {
  * user agent of sdk
  * @hidden
  */
-export const USER_AGENT = ["sajari-sdk-js/2.2.0", suffix]
+export const USER_AGENT = ["sajari-sdk-js/2.3.0", suffix]
   .filter(Boolean)
   .join(" ");
