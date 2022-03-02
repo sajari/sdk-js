a8656b4a9cb74403f46fa778279a422d80ea1e36
Jason Berry <jberry@search.io>
2022-03-02 18:37:55 +1100

    release: version 2.10.2

--- src/user-agent.ts
+++ src/user-agent.ts
@@ -16,6 +16,6 @@ if (scriptTag) {
  * user agent of sdk
  * @hidden
  */
-export const USER_AGENT = ["sajari-sdk-js/2.10.0", suffix]
+export const USER_AGENT = ["sajari-sdk-js/2.10.2", suffix]
   .filter(Boolean)
   .join(" ");
