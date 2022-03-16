bf39d363f23f6ff7e3a5af1550aed64478880e02
Jason Berry <jberry@search.io>
2022-03-16 15:38:19 +1100

    docs: deprecate click and posneg tracking

--- src/index.ts
+++ src/index.ts
@@ -58,6 +58,7 @@ type ConfigObj = {
   /**
    * Redirect URL for click tracking.  Prepended to the the front of the token
    * returned by the engine.
+   * @deprecated
    */
   clickTokenURL: string;
   /**
@@ -215,6 +216,7 @@ export class Client {
 
   /**
    * interactionConsume consumes an interaction token.
+   * @deprecated
    */
   async interactionConsume(
     token: string,
@@ -664,6 +666,7 @@ export interface Result {
   values: Record<string, string | string[]>;
   /**
    * token is the [[Token]] associated with this [[Result]] (if any).
+   * @deprecated
    */
   token?: Token;
   /**
@@ -672,15 +675,20 @@ export interface Result {
   promotionPinned?: boolean;
 }
 
+/**
+ * @deprecated
+ */
 export type Token = ClickToken | PosNegToken;
 
 /**
  * ClickToken defines a click token.  See [[TrackingType.Click]] for more details.
+ * @deprecated
  */
 export type ClickToken = { click: string };
 
 /**
  * PosNegToken defines a pos/neg token pair. See [[TrackingType.PosNeg]] for more details.
+ * @deprecated
  */
 export type PosNegToken = { pos: string; neg: string };
 
@@ -878,6 +886,7 @@ interface MetricAggregateProto {
 
 /**
  * Tracking defines behaviour for handling search sessions and result interactions.
+ * @deprecated
  */
 export type Tracking = {
   type: TrackingType;
@@ -908,10 +917,12 @@ export enum TrackingType {
   None = "NONE",
   /**
    * Click generates click tracking tokens.
+   * @deprecated
    */
   Click = "CLICK",
   /**
    * PosNeg creates pos/neg tracking tokens.
+   * @deprecated
    */
   PosNeg = "POS_NEG",
   /**
@@ -923,6 +934,7 @@ export enum TrackingType {
 /**
  * Session takes query values, maintains session state, and returns tracking data
  * to be sent with search requests.
+ * @deprecated
  */
 export interface Session {
   /**
@@ -941,6 +953,7 @@ export const EVENT_TRACKING_RESET = "tracking-reset";
 
 /**
  * DefaultSession holds state of a sequence of searches.
+ * @deprecated
  */
 export class DefaultSession extends EventEmitter implements Session {
   private queryID: string = "";
@@ -1044,6 +1057,7 @@ function newQueryID(len: number = 16): string {
  * - Is `undefined`.
  * - First 3 characters have changed (i.e. from a direct replacement)
  * - Cleared after being non-empty (i.e. from a delete)
+ * @deprecated
  */
 export class InteractiveSession implements Session {
   private session: Session;
@@ -1236,6 +1250,9 @@ export class Values extends EventEmitter {
   }
 }
 
+/**
+ * @deprecated
+ */
 export type TokenState = {
   token: PosNegToken;
   clickSubmitted: boolean;
@@ -1264,6 +1281,7 @@ export const getItem = (key: string): string | null => {
  * PosNegLocalStorageManager is a utility class for manipulating Sajari's localstorage based
  * management of PosNeg tokens. Typical use case is storing tokens for later consumption
  * as users move through an ecommerce purchase funnel.
+ * @deprecated
  */
 export class PosNegLocalStorageManager {
   currentTokens: Record<string | number, TokenState>;
