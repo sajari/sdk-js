59b07450d05c24f5502a257e5437eac9c42216bf
Ben Hinchley <7188324+benhinchley@users.noreply.github.com>
2022-06-23 04:35:49 +0000

    remove jest-fetch-mock (take 2)

--- src/util.ts
+++ src/util.ts
@@ -1,21 +1,5 @@
 import { isSSR } from "./ssr";
 
-/**
- * RequestError defines an error occuring from a request.
- */
-export class RequestError extends Error {
-  constructor(
-    public readonly statusCode: number,
-    message: string,
-    public readonly error?: Error
-  ) {
-    super(message);
-
-    // TODO(jingram): Remove this when compilation target is higher than ES5.
-    Object.setPrototypeOf(this, RequestError.prototype);
-  }
-}
-
 // Having a proxy helps enables SSR execution (docs)
 export const setItem = (key: string, value: string) => {
   if (isSSR()) {
